6246b020219798d4e34d6d38aa4f862d235845a4
Likai Kuroi <likai31197@hotmail.com>
2018-11-26 18:21:35 -0500

    tidying up

--- inotify.c
+++ inotify.c
@@ -7,7 +7,7 @@
 #include <string.h>
 /*
 We will be using the inotify function in Linux to monitor changes in a directory, and store them in a log.
-Changes we will monitor: file creation, deletions, and, renamkes under the directory named in its command-line argument.
+Changes we will monitor: file creation, deletions, and, renamees under the directory named in its command-line argument.
 Program will run in the background and monitor/write events to log.
 
 http://man7.org/linux/man-pages/man7/inotify.7.html
@@ -54,7 +54,7 @@ int notify(char *directory, int buffer_length, char *log_directory){
 	int watch_descriptor = inotify_add_watch(file_descriptor,
 					   	   ".", IN_MODIFY | IN_CREATE | IN_DELETE);// http://man7.org/linux/man-pages/man2/inotify_add_watch.2.html
 															   	   // IN events are defined in inotify.h - https://code.woboq.org/qt5/include/sys/inotify.h.html
-	int data_length = read(file_descriptor, buffer, buffer_length); // length of the data read for file or directory
+	int data_length = read(file_descriptor, buffer, buffer_length);// length of the data read for file or directory
 	
 	if(data_length < 0) {
 		perror("read");	
