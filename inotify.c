b2fd0549930faf434039533996c46a0b93dec143
Likai Kuroi <likai31197@hotmail.com>
2018-11-26 18:07:37 -0500

    constant monitoring + logging w/ timestamps

--- inotify.c
+++ inotify.c
@@ -3,7 +3,8 @@
 #include <sys/types.h>
 #include <sys/inotify.h>
 #include <unistd.h>
-
+#include <time.h>
+#include <string.h>
 /*
 We will be using the inotify function in Linux to monitor changes in a directory, and store them in a log.
 Changes we will monitor: file creation, deletions, and, renamkes under the directory named in its command-line argument.
@@ -19,7 +20,31 @@ http://man7.org/linux/man-pages/man2/inotify_add_watch.2.html
 #define BUFFER_LEN	(1024 * (EVENT_SIZE + 16))
 
 
-int notify(char *directory, int buffer_length){
+//https://stackoverflow.com/questions/16443780/how-to-return-a-char-array-from-a-function-in-c/16444218
+char *timestamp(){
+    time_t ltime; /* calendar time */
+    ltime=time(NULL); /* get current cal time */
+    return asctime( localtime(&ltime) );
+}
+
+// Receives a path and data
+int log_output(char *path, char *content){
+	FILE * file_pointer; // pointer to a file, in this case, our log file.
+	file_pointer = fopen(path, "a+");
+	printf("%s\n", path);
+	printf("%s\n", content);
+	if(file_pointer==NULL) {
+    	perror("Error opening log file.");
+		return -1;
+	}
+	
+	fprintf(file_pointer, "%s", content);
+	fclose(file_pointer);
+	
+	return 0;	
+}
+
+int notify(char *directory, int buffer_length, char *log_directory){
 	char buffer[buffer_length];
 	int file_descriptor = inotify_init();
 	
@@ -28,7 +53,7 @@ int notify(char *directory, int buffer_length){
 	}
 	int watch_descriptor = inotify_add_watch(file_descriptor,
 					   	   ".", IN_MODIFY | IN_CREATE | IN_DELETE);// http://man7.org/linux/man-pages/man2/inotify_add_watch.2.html
-															   // IN events are defined in inotify.h - https://code.woboq.org/qt5/include/sys/inotify.h.html
+															   	   // IN events are defined in inotify.h - https://code.woboq.org/qt5/include/sys/inotify.h.html
 	int data_length = read(file_descriptor, buffer, buffer_length); // length of the data read for file or directory
 	
 	if(data_length < 0) {
@@ -40,11 +65,17 @@ int notify(char *directory, int buffer_length){
 		struct inotify_event *event = (struct inotify_event *) &buffer[i];
 		if(event->len) {
 			if(event->mask & IN_CREATE) {
-				printf("%s was created.\n", event->name);	
+				printf("%s was created.\n", event->name);
+				log_output(log_directory, strcat(timestamp(), strcat(event->name," was created.\n")));
+
 			} else if(event->mask & IN_DELETE) {
-				printf("%s was deleted.\n", event->name);	
+				printf("%s was deleted.\n", event->name);
+				log_output(log_directory, strcat(timestamp(),strcat(event->name," was deleted.\n")));
+
 			} else if(event->mask & IN_MODIFY) {
 				printf("%s was modified.\n", event->name);	
+				log_output(log_directory, strcat(timestamp(), strcat(event->name," was modified.\n")));
+
 			}
 		}
 		i += EVENT_SIZE + event->len;
@@ -56,16 +87,27 @@ int notify(char *directory, int buffer_length){
 	return 0;
 }
 
-
+// Arg 0 = program name
+// Arg 1 = monitoring directory
+// Arg 2 = log directory
+// 1 and 2 cannot be equal.
 int main(int argc, char **argv){
 	// Handle the arguments.
-	if(argc < 2){
-		printf("Not enough args. See usage: inotify directory_here");
+	if(argc < 3){
+		printf("Not enough args. See usage: inotify monitor_directory log_directory_incl_filename\n");
+		printf("Another example: ./inotify \"home/user/directory\" \"home/user/log/log.txt\"");
 		return -1;
-	} else if(argc == 2){
-		notify(argv[1], BUFFER_LEN);	
+	} else if(argc == 3){
+		if(strcmp(argv[1],argv[2])==0){ // if the monitor directory is the same as the log directory, don't allow!
+			printf("You cannot have the log directory be in the directory you are monitoring! :c\n");
+			return -1;
+		}
+		while(1){
+			notify(argv[1], BUFFER_LEN, argv[2]);	
+		}
 	} else{
-		printf("Too many args. See usage: inotify directory_here");	
+		printf("Too many args. See usage: inotify monitor_directory log_directory\n");
+		printf("Another example: ./inotify \"home/user/directory\" \"home/user/log/log.txt\"");
 		return -1;
 	}
 	
